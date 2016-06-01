f30f70051222f461bd7b68273f4037deb331569b
Andy Aschwanden <aaschwanden@alaska.edu>
2016-06-01 10:10:36 +0200

    updated resources for plastic_phi

--- resources/resources.py
+++ resources/resources.py
@@ -371,8 +371,7 @@ def generate_stress_balance(stress_balance, additional_params_dict):
         params_dict['pseudo_plastic'] = ''
         params_dict['pseudo_plastic_q'] = additional_params_dict['pseudo_plastic_q']
         params_dict['till_effective_fraction_overburden'] = additional_params_dict['till_effective_fraction_overburden']
-        params_dict['topg_to_phi'] = additional_params_dict['topg_to_phi']
-        params_dict['tauc_slippery_grounding_lines'] = ''
+        params_dict['plastic_phi'] = additional_params_dict['plastic_phi']
     return merge_dicts(additional_params_dict, params_dict)
 
 
