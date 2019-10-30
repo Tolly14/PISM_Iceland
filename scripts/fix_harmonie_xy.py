6ca1a8fa2e6677ab1f6773c9110b92e18f43a558
Andy Aschwanden <aaschwanden@alaska.edu>
2019-10-29 16:15:59 -0800

    Fixed HARMONIE projection

--- scripts/fix_harmonie_xy.py
+++ scripts/fix_harmonie_xy.py
@@ -4,9 +4,8 @@ from netCDF4 import Dataset as NC
 import numpy as np
 from pyproj import Proj
 
-p = Proj("+init=epsg:3057")
-
-xm, ym = p(-19., 64.7)
+p = Proj("+proj=lcc +lat_0=64.7 +lon_0=-19 +lat_1=64.7 +lat_2=64.7 +x_0=0 +y_0=0 +ellps=GRS80 +units=m +no_defs")
+xm, ym = 359.451, 309.670
 
 # set up the argument parser
 parser = ArgumentParser()
@@ -18,10 +17,9 @@ ne = 289
 nn = 229
 dx = 2500
 
-easting = np.linspace(xm - (ne - 1) / 2  * dx, xm + (ne - 1) / 2  * dx, ne)
-northing = np.linspace(ym - (nn - 1) / 2  * dx, ym + (nn - 1) / 2 * dx, nn)
+easting = np.linspace(xm - (ne - 1) / 2 * dx, xm + (ne - 1) / 2 * dx, ne)
+northing = np.linspace(ym - (nn - 1) / 2 * dx, ym + (nn - 1) / 2 * dx, nn)
 
-print(easting[0], northing[0])
 
 nc = NC(options.FILE[0], "a")
 
@@ -56,16 +54,15 @@ if "mapping" not in nc.variables:
     mapping = nc.createVariable("mapping", "c")
 else:
     mapping = nc.variables["mapping"]
-    
-mapping.grid_mapping_name = "lambert_conformal_conic" ;
-mapping.longitude_of_central_meridian = -19.
-mapping.false_easting = 500000.
-mapping.false_northing = 500000.
-mapping.latitude_of_projection_origin = 65.
-mapping.standard_parallel = [64.25, 65.75]
-mapping.longitude_of_prime_meridian = 0.
-mapping.semi_major_axis = 6378137.
-mapping.inverse_flattening = 298.257222101
 
+mapping.grid_mapping_name = "lambert_conformal_conic"
+mapping.longitude_of_central_meridian = -19.0
+mapping.false_easting = 0.0
+mapping.false_northing = 0.0
+mapping.latitude_of_projection_origin = 64.7
+mapping.standard_parallel = 64.7
+mapping.longitude_of_prime_meridian = 0.0
+mapping.semi_major_axis = 6378137.0
+mapping.inverse_flattening = 298.257222101
 
 nc.close()
